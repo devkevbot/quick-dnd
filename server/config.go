fa48be2f4d144c92f2c6636e675f191683b8f76c
knsjerome <kjerome@sfu.ca>
2021-02-27 23:30:34 -0800

    Add configuration code to connect to a database
    Created new helper methods which attempt to read in a YAML file that
    holds configuration data.
    
    Once read, the configuration is then parsed to create a DB connection
    string and a connection is initiated to the database.
    
    Exits immediately if any of these operations fail.

---
+++ server/config.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"gopkg.in/yaml.v2"
+)
+
+// Config holds important configuration data, such as database
+// credentials. In the future, this type may be expanded to hold server
+// configuration data as well.
+type Config struct {
+	Database struct {
+		Username string `yaml:"user"`
+		Password string `yaml:"pass"`
+		DBName   string `yaml:"dbname"`
+		Host     string `yaml:"host"`
+		Port     string `yaml:"port"`
+	} `yaml:"database"`
+}
+
+// CreatePostgreSQLDBConnString returns a formatted string used the
+// database present in `c`. Uses `sslmode` is `useSSL` is set to true.
+func (c Config) CreatePostgreSQLDBConnString(useSSL bool) string {
+	sslmode := "disable"
+	if useSSL {
+		sslmode = "enable"
+	}
+
+	return fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
+		c.Database.Host,
+		c.Database.Port,
+		c.Database.Username,
+		c.Database.Password,
+		c.Database.DBName,
+		sslmode,
+	)
+}
+
+func createConfigFromFile(path string) *Config {
+	file, err := os.Open(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	c := Config{}
+	if err := yaml.NewDecoder(file).Decode(&c); err != nil {
+		log.Fatal(err)
+	}
+	return &c
+}
--- server/main.go
+++ server/main.go
@@ -2,21 +2,31 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"net/http"
 	"strconv"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+
+	_ "github.com/lib/pq"
 )
 
 func main() {
 	portPtr := flag.Int("p", 8080, "Port Number")
+	configFile := flag.String("cfg", "config.yml", "Configuration YAML File")
 	flag.Parse()
 
 	e := echo.New()
 
 	e.Use(middleware.Recover())
 	e.Use(middleware.Logger())
+
+	cfg := createConfigFromFile(*configFile)
+
+	db := initDBConn(cfg.CreatePostgreSQLDBConnString(false))
+	fmt.Println(db)
+
 	e.GET("/", func(c echo.Context) error {
 		return c.HTML(http.StatusOK, `
 			<h1>Hello World!</h1>
