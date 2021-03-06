472475f432c239ad682b3157feffefff1848189b
knsjerome <31908183+knsjerome@users.noreply.github.com>
2021-03-06 15:13:49 -0800

    Setup basic gui (#15)
    * Remove boilerplate GUI code
    
    * Create GUI starting point
    
    Created the app layout: the app bar, navigation drawer, content, and
    footer.
    
    Created a simple timeline feature to better visualize a campaign
    tracking feature.

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,56 +1,60 @@
 <template>
   <v-app>
-    <v-app-bar app color="primary" dark>
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
+    <v-card>
+      <v-navigation-drawer app color="secondary" permanent clipped>
+        <v-list>
+          <v-list-item v-for="item in items" :key="item.title" link>
+            <v-list-item-icon>
+              <v-icon color="accent">{{ item.icon }}</v-icon>
+            </v-list-item-icon>
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
+            <v-list-item-content>
+              <v-list-item-title>{{ item.title }}</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
 
+        <template v-slot:append>
+          <div class="pa-2">
+            <v-btn block color="accent"> Logout </v-btn>
+          </div>
+        </template>
+      </v-navigation-drawer>
+    </v-card>
+
+    <v-app-bar app color="primary" dark clipped-left flat>
+      <v-icon class="mr-4">mdi-home</v-icon>
+      <v-toolbar-title>QuickDND</v-toolbar-title>
       <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
     </v-app-bar>
 
     <v-main>
-      <HelloWorld />
+      <v-container fluid>
+        <router-view></router-view>
+      </v-container>
     </v-main>
+
+    <v-footer app color="primary" padless>
+      <v-row justify="center" no-gutters>
+        <v-col class="primary py-4 text-center white--text" cols="12">
+          {{ new Date().getFullYear() }} — <strong>QuickDND</strong>
+        </v-col>
+      </v-row>
+    </v-footer>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
-
 export default {
   name: 'App',
-
-  components: {
-    HelloWorld,
+  data() {
+    return {
+      items: [
+        { title: 'Home', icon: 'mdi-view-dashboard' },
+        { title: 'My profile', icon: 'mdi-account-box' },
+        { title: 'Create a character', icon: 'mdi-gavel' },
+      ],
+    };
   },
-
-  data: () => ({
-    //
-  }),
 };
 </script>
