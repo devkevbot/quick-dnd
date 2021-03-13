486a53a8759067340977b4a2adc27433f11a12fd
knsjerome <kjerome@sfu.ca>
2021-03-13 14:04:07 -0800

    Update App.vue to render new page setup

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,60 +1,31 @@
+<!-- App.vue
+This component is the starting place for all other component rendering.
+Layouts are computed and shown. The main content of the page is
+determined from the current page route and shown by <router-view>.
+-->
+
 <template>
-  <v-app>
-    <v-card>
-      <v-navigation-drawer app color="secondary" permanent clipped>
-        <v-list>
-          <v-list-item v-for="item in items" :key="item.title" link>
-            <v-list-item-icon>
-              <v-icon color="accent">{{ item.icon }}</v-icon>
-            </v-list-item-icon>
-
-            <v-list-item-content>
-              <v-list-item-title>{{ item.title }}</v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
-        </v-list>
-
-        <template v-slot:append>
-          <div class="pa-2">
-            <v-btn block color="accent"> Logout </v-btn>
-          </div>
-        </template>
-      </v-navigation-drawer>
-    </v-card>
-
-    <v-app-bar app color="primary" dark clipped-left flat>
-      <v-icon class="mr-4">mdi-home</v-icon>
-      <v-toolbar-title>QuickDND</v-toolbar-title>
-      <v-spacer></v-spacer>
-    </v-app-bar>
-
-    <v-main>
-      <v-container fluid>
-        <router-view></router-view>
-      </v-container>
-    </v-main>
-
-    <v-footer app color="primary" padless>
-      <v-row justify="center" no-gutters>
-        <v-col class="primary py-4 text-center white--text" cols="12">
-          {{ new Date().getFullYear() }} — <strong>QuickDND</strong>
-        </v-col>
-      </v-row>
-    </v-footer>
+  <v-app id="app">
+    <component :is="layout">
+      <router-view />
+    </component>
   </v-app>
 </template>
 
 <script>
+import MainLayout from './layouts/MainLayout.vue';
+import GuestLayout from './layouts/GuestLayout.vue';
+
 export default {
   name: 'App',
-  data() {
-    return {
-      items: [
-        { title: 'Home', icon: 'mdi-view-dashboard' },
-        { title: 'My profile', icon: 'mdi-account-box' },
-        { title: 'Create a character', icon: 'mdi-gavel' },
-      ],
-    };
+  components: {
+    MainLayout,
+    GuestLayout,
+  },
+  computed: {
+    layout() {
+      return this.$route.meta.layout === 'Main' ? MainLayout : GuestLayout;
+    },
   },
 };
 </script>
