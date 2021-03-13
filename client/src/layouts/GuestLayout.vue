608fdb8d8d873418d839a8a41be27b754fe7e785
knsjerome <kjerome@sfu.ca>
2021-03-13 14:01:25 -0800

    Add layouts for guest and signed-in users
    Creates layouts which should be used dynamically depending on whether or
    not a user is signed in.

---
+++ client/src/layouts/GuestLayout.vue
@@ -0,0 +1,26 @@
+<!-- GuestLayout.vue
+This file is one of several layouts used in the website. This layout
+should be shown to unauthenticated users (users who are not signed-in).
+-->
+
+<template>
+  <div class="guest-layout">
+    <v-main>
+      <v-container fluid>
+        <slot />
+      </v-container>
+    </v-main>
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Footer from './Footer.vue';
+
+export default {
+  name: 'GuestLayout',
+  components: {
+    Footer,
+  },
+};
+</script>
