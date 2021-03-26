13734e33ea92b193d4db088f9eb12128352541b0
Dylan Rowsell <dylanrowsell@Dylans-MacBook-Pro.local>
2021-03-26 10:05:31 -0700

    WIP - Adding Database Values to UI

--- client/src/views/Characters.vue
+++ client/src/views/Characters.vue
@@ -7,8 +7,39 @@ Currently a work in progress.
 <template>
 <div class="characters">
 
+<template>
+   <div class="stat-card">
+     <v-container class="mx-2">
+       <v-row>
+         <v-col cols="4">
+           <v-text-field
+             v-model="characterID"
+             outlined
+             label="Search for a character by ID"
+             placeholder="Enter a numeric character ID"
+             type="text"
+             append-icon="mdi-send"
+             @click:append="fetchCharacter(characterID)"
+             @keyup.enter="fetchCharacter(characterID)"
+           >
+           </v-text-field>
+         </v-col>
+       </v-row>
+       <v-row>
+         <v-card flat>
+           <v-card-title>Results:</v-card-title>
+           <v-card-text>
+             <pre>{{ characterDataJSON }}</pre>
+             <pre>{{ characterData.id }}</pre>
+           </v-card-text>
+         </v-card>
+       </v-row>
+     </v-container>
+   </div>
+ </template>
+
   <v-btn>
-    Temp Save Button
+    <h3> SAVE </h3>
   </v-btn>
 
   <v-container class="campaign">
@@ -68,7 +99,7 @@ Currently a work in progress.
                 {{statUniqueElements[1].attribute}}
               </v-card-title>
               <v-card-actions>
-                <v-text-field label="Level 24" solo disabled>
+                <v-text-field v-model="statUniqueElements[1].value" label="TEMP" solo disabled>
                 </v-text-field>
               </v-card-actions>
             </v-card>
@@ -79,7 +110,7 @@ Currently a work in progress.
                 {{statUniqueElements[3].attribute}}
               </v-card-title>
               <v-card-actions>
-                <v-text-field label="5000" filled>
+                <v-text-field v-model="statUniqueElements[3].value" label="5000" filled>
                 </v-text-field>
               </v-card-actions>
             </v-card>
@@ -128,7 +159,7 @@ Currently a work in progress.
               {{row.attribute}}
             </v-card-title>
             <v-card-actions>
-              <v-text-field label="Temp" solo disabled>
+              <v-text-field v-model="row.value" label="Temp" solo disabled>
               </v-text-field>
             </v-card-actions>
           </v-card>
@@ -145,10 +176,11 @@ Currently a work in progress.
               </v-card-title>
               <v-card-actions>
                 <v-textarea
+                  v-model="statUniqueElements[2].value"
                   filled
                   name="input-7-4"
                   label="Filled textarea"
-                  value="Filled text."
+                  value="statUniqueElements[2].value will go here"
                 ></v-textarea>
               </v-card-actions>
             </v-card>
@@ -164,9 +196,9 @@ Currently a work in progress.
                 {{row.attribute}}
               </v-card-title>
               <v-card-actions>
-                <v-slider v-model="statSliderValue" :max="100" class="align-center">
+                <v-slider v-model="row.value" :max="100" class="align-center">
                   <template v-slot:append>
-                    <v-text-field v-model="statSliderValue" class="mt-0 pt-0" type="text">
+                    <v-text-field v-model="row.value" class="mt-0 pt-0" type="text">
                     </v-text-field>
                   </template>
                 </v-slider>
@@ -184,7 +216,7 @@ Currently a work in progress.
               {{row.modifier}}
             </v-card-title>
             <v-card-actions>
-              <v-text-field label="Temp" solo disabled>
+              <v-text-field v-model="row.value" label="TEMP" solo disabled>
               </v-text-field>
             </v-card-actions>
           </v-card>
@@ -198,54 +230,87 @@ Currently a work in progress.
 export default {
   data() {
     return {
-      statSlider: [
-        { attribute: 'Strength' },
-        { attribute: 'Contribution' },
-        { attribute: 'Wisdom' },
-        { attribute: 'Hit Points' },
-        { attribute: 'Dexterity' },
-        { attribute: 'Ability_Points' },
-        { attribute: 'Charisma' },
-        { attribute: 'Intellect' },
-      ],
-      statSelect: [
-        { attribute: 'Campaign' },
-        { attribute: 'State' },
-        { attribute: 'Character Name' },
-        { attribute: 'Abilities' },
-        { attribute: 'Items' },
-        { attribute: 'Spells' },
-        { attribute: 'Alignment' },
-      ],
-      characterTraits: [
-        { attribute: 'Race' },
-        { attribute: 'Class' },
-        { attribute: 'Weight' },
-        { attribute: 'Height' },
-        { attribute: 'Sex' },
-        { attribute: 'Armour Class' },
-      ],
-      statModifiers: [
-        { modifier: 'Initiative' },
-        { modifier: 'Number of Dies' },
-        { modifier: 'Strength' },
-        { modifier: 'Constitution' },
-        { modifier: 'Wisdom' },
-        { modifier: 'Hit Points' },
-        { modifier: 'Dexterity' },
-        { modifier: 'Ability Points' },
-        { modifier: 'Charisma' },
-        { modifier: 'Intellect' },
-        { modifier: 'Proficency' },
-        { modifier: 'Speed' },
-      ],
-      statUniqueElements: [
-        { attribute: 'Location' },
-        { attribute: 'Level' },
-        { attribute: 'Character Background' },
-        { attribute: 'XP' },
-      ],
+      characterID: undefined,
+      characterDataJSON: {},
+      characterData: {},
     };
+  },
+  computed: {
+    characterTraits() {
+      return [
+        { attribute: 'Race', value: this.characterData.race },
+        { attribute: 'Class', value: this.characterData.class },
+        { attribute: 'Weight', value: this.characterData.weight },
+        { attribute: 'Height', value: this.characterData.height },
+        { attribute: 'Sex', value: this.characterData.sex },
+        { attribute: 'Armour Class', value: 'TEMP' },
+      ];
+    },
+    statSlider() {
+      return [
+        { attribute: 'Strength', value: this.characterData.strength },
+        { attribute: 'Constitution', value: this.characterData.constitution },
+        { attribute: 'Wisdom', value: this.characterData.wisdom },
+        { attribute: 'Hit Points', value: this.characterData.hp_max },
+        { attribute: 'Dexterity', value: this.characterData.dexterity },
+        { attribute: 'Ability Points', value: this.characterData.ability_points },
+        { attribute: 'Charisma', value: this.characterData.charisma },
+        { attribute: 'Intellect', value: this.characterData.intelligence },
+      ];
+    },
+    statSelect() {
+      return [
+        { attribute: 'Campaign', value: 'TEMP' },
+        { attribute: 'State', value: 'TEMP' },
+        { attribute: 'Character Name', value: this.characterData.name },
+        { attribute: 'Abilities', value: 'TEMP' },
+        { attribute: 'Items', value: 'TEMP' },
+        { attribute: 'Spells', value: 'TEMP' },
+        { attribute: 'Alignment', value: this.characterData.alignment },
+      ];
+    },
+    statModifiers() {
+      return [
+        { modifier: 'Initiative', value: 'TEMP' },
+        { modifier: 'Number of Dies', value: 'TEMP' },
+        { modifier: 'Strength', value: 'TEMP' },
+        { modifier: 'Constitution', value: 'TEMP' },
+        { modifier: 'Wisdom', value: 'TEMP' },
+        { modifier: 'Hit Points', value: 'TEMP' },
+        { modifier: 'Dexterity', value: 'TEMP' },
+        { modifier: 'Ability Points', value: 'TEMP' },
+        { modifier: 'Charisma', value: 'TEMP' },
+        { modifier: 'Intellect', value: 'TEMP' },
+        { modifier: 'Proficency', value: 'TEMP' },
+        { modifier: 'Speed', value: 'TEMP' },
+      ];
+    },
+    statUniqueElements() {
+      return [
+        { attribute: 'Location', value: 'TEMP' },
+        { attribute: 'Level', value: 'TEMP' },
+        { attribute: 'Character Background', value: this.characterData.background },
+        { attribute: 'XP', value: this.characterData.xp_points },
+      ];
+    },
+  },
+  methods: {
+    async fetchCharacter(id) {
+      const numericID = parseInt(id, 10);
+      const requestURI = `/character/${numericID}`;
+      const method = 'GET';
+
+      await this.$http({ url: requestURI, data: null, method })
+        .then((resp) => {
+          this.characterDataJSON = JSON.stringify(resp.data.data, null, '\t');
+          // this.characterData = JSON.parse(this.characterDataJSON);
+          this.characterData = resp.data.data;
+        })
+        .catch(() => {
+          const msg = { error: 'character not found' };
+          this.characterDataJSON = JSON.stringify(msg, null, '\t');
+        });
+    },
   },
 };
 </script>
