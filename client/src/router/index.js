0a3d696363e0900804205fc0d14a7392900dda1f
knsjerome <kjerome@sfu.ca>
2021-04-02 13:19:17 -0700

    Remove home page
    This page is replaced by the dashboard page.

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,19 +1,13 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import store from '@/store/index';
-import Home from '../views/Home.vue';
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home,
-    meta: {
-      layout: 'Main',
-      protected: true,
-    },
+    redirect: '/dashboard',
   },
   {
     path: '/register',
