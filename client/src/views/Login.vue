7f6b3cf35595d9a45004c24bade32e161f07c708
knsjerome <31908183+knsjerome@users.noreply.github.com>
2021-04-03 23:13:06 -0700

    Add stats banner to login page. (#72)
    Added a banner component which displays some stats from the application.
    This banner pulls data from the SQL triggers that tick when a new
    player, character, etc is created.

--- client/src/views/Login.vue
+++ client/src/views/Login.vue
@@ -4,15 +4,18 @@ This component is the view shown when a user is on the login page.
 
 <template>
   <div class="login">
+    <StatsBanner />
     <LoginForm />
   </div>
 </template>
 
 <script>
+import StatsBanner from '../components/login/StatsBanner.vue';
 import LoginForm from '../components/login/LoginForm.vue';
 
 export default {
   components: {
+    StatsBanner,
     LoginForm,
   },
 };
