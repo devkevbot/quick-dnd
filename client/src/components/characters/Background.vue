7b892f16629a7a909585b6d18212be5f7d904b4f
knsjerome <kjerome@sfu.ca>
2021-04-05 21:39:26 -0700

    Create "Background" step for character creation
    Add new component that captures data related to a character's background
    when during the character creation process.
    
    This component will be rendered as the first step of a creation stepper
    process.

---
+++ client/src/components/characters/Background.vue
@@ -0,0 +1,145 @@
+<template>
+  <v-form>
+    <v-container>
+      <!-- Character name, sex, race. -->
+      <p class="headline primary--text">Identity</p>
+      <v-row>
+        <v-col cols="12" md="2">
+          <v-text-field
+            v-model.trim="character.name"
+            label="Enter name"
+            outlined
+            required
+            placeholder="Trogdor, Destroyer of Worlds"
+          >
+          </v-text-field>
+        </v-col>
+
+        <v-col cols="12" md="2">
+          <v-select
+            v-model="character.sex"
+            :items="characterSexOptions"
+            label="Choose sex"
+            outlined
+            required
+          >
+          </v-select>
+        </v-col>
+
+        <v-col cols="12" md="2">
+          <v-select
+            v-model.trim="character.race"
+            :items="characterRaceOptions"
+            label="Choose race"
+            outlined
+            required
+          >
+          </v-select>
+        </v-col>
+      </v-row>
+
+      <!-- Character weight and height. -->
+      <p class="headline primary--text">Body Shape</p>
+      <v-row>
+        <v-col cols="12" md="3">
+          <v-text-field
+            v-model.trim="character.weight"
+            label="Enter weight"
+            suffix="lbs"
+            outlined
+            required
+          >
+          </v-text-field>
+        </v-col>
+
+        <v-col cols="12" md="3">
+          <v-text-field
+            v-model.trim="character.height"
+            label="Enter height"
+            suffix="ft"
+            outlined
+            required
+          >
+          </v-text-field>
+        </v-col>
+      </v-row>
+
+      <p class="headline primary--text">Alignment</p>
+      <v-row>
+        <v-col cols="12" md="6">
+          <v-select
+            v-model.trim="character.alignment"
+            :items="characterAlignmentOptions"
+            label="Choose alignment"
+            outlined
+            required
+          >
+          </v-select>
+        </v-col>
+      </v-row>
+
+      <!-- Character background. -->
+      <p class="headline primary--text">Background</p>
+      <v-row>
+        <v-col cols="12" md="6">
+          <v-textarea
+            v-model.trim="character.background"
+            label="Enter background"
+            placeholder="A dark and mysterious past."
+            outlined
+            clearable
+            no-resize
+            required
+            counter="250"
+          >
+          </v-textarea>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-form>
+</template>
+
+<script>
+export default {
+  name: 'Background',
+  data() {
+    return {
+      character: {
+        name: null,
+        sex: 'Male',
+        race: 'Human',
+        weight: 180,
+        height: 6.2,
+        alignment: 'True Neutral',
+        background: 'A dark and mysterious past.',
+      },
+
+      characterSexOptions: ['Male', 'Female', 'Other'],
+
+      characterRaceOptions: [
+        'Dragonborn',
+        'Dwarf',
+        'Elf',
+        'Gnome',
+        'Half-Elf',
+        'Halfling',
+        'Half-Orc',
+        'Human',
+        'Tiefling',
+      ],
+
+      characterAlignmentOptions: [
+        'Lawful Good',
+        'Neutral Good',
+        'Chaotic Good',
+        'Lawful Neutral',
+        'True Neutral',
+        'Chaotic Neutral',
+        'Lawful Evil',
+        'Neutral Evil',
+        'Chaotic Evil',
+      ],
+    };
+  },
+};
+</script>
