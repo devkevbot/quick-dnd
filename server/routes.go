671327f83f0b53b69336c29319bcb81de1735686
knsjerome <kjerome@sfu.ca>
2021-04-10 23:34:46 -0700

    Add new query to fullfill SQL division requirement
    Create new endpoint `/auth/campaign/me/stats/player-attendance` which
    fetches a list of player usernames that have participated in all
    campaigns created by the requestor.
    
    This endpoint utilizes a query which should fulfill the SQL division
    requirement for the project.

--- server/routes.go
+++ server/routes.go
@@ -41,4 +41,5 @@ func (app *application) registerRoutes(e *echo.Echo) {
 
 	// Protected campaign endpoints
 	r.POST("/campaign", app.createCampaign)
+	r.GET("/campaign/me/stats/player-attendance", app.getPlayersAttendedAll)
 }
