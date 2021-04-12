d948f8b5712a547ef78932281a106885ef57607e
knsjerome <31908183+knsjerome@users.noreply.github.com>
2021-04-11 22:18:45 -0700

    Validate user inputs during character creation (#110)
    * Add validation for class step of character create
    
    * Add validation for BG step of character create

--- client/src/components/characters/creation/Background.vue
+++ client/src/components/characters/creation/Background.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-form>
+  <v-form @submit.prevent>
     <v-container>
       <!-- Character name, sex, race. -->
       <p class="headline primary--text">Identity</p>
@@ -11,6 +11,9 @@
             outlined
             required
             placeholder="Trogdor, Destroyer of Worlds"
+            :error-messages="nameErrors"
+            @input="$v.character.name.$touch()"
+            @blur="$v.character.name.$touch()"
           >
           </v-text-field>
         </v-col>
@@ -48,6 +51,9 @@
             suffix="kg"
             outlined
             required
+            :error-messages="weightErrors"
+            @input="$v.character.weight.$touch()"
+            @blur="$v.character.weight.$touch()"
           >
           </v-text-field>
         </v-col>
@@ -59,6 +65,9 @@
             suffix="cm"
             outlined
             required
+            :error-messages="heightErrors"
+            @input="$v.character.height.$touch()"
+            @blur="$v.character.height.$touch()"
           >
           </v-text-field>
         </v-col>
@@ -84,13 +93,14 @@
         <v-col cols="12" md="6">
           <v-textarea
             v-model.trim="character.background"
-            label="Enter background"
+            label="Enter background (optional)"
             placeholder="A dark and mysterious past."
             outlined
-            clearable
             no-resize
-            required
             counter="250"
+            :error-messages="backgroundErrors"
+            @input="$v.character.background.$touch()"
+            @blur="$v.character.background.$touch()"
           >
           </v-textarea>
         </v-col>
@@ -102,6 +112,16 @@
 </template>
 
 <script>
+import {
+  required,
+  minLength,
+  maxLength,
+  integer,
+  minValue,
+  maxValue,
+  helpers,
+} from 'vuelidate/lib/validators';
+
 export default {
   name: 'Background',
   data() {
@@ -143,12 +163,104 @@ export default {
       ],
     };
   },
+  validations: {
+    character: {
+      name: {
+        required,
+        minLength: minLength(1),
+        maxLength: maxLength(50),
+        /* This regex obviously does not cover all cases, but is
+      sufficient for our project. */
+        isNameValid: helpers.regex('nameValidation', /^[a-zA-Z, '.-]*$/),
+      },
+      weight: {
+        required,
+        integer,
+        minValue: minValue(1),
+        maxValue: maxValue(1000),
+      },
+      height: {
+        required,
+        integer,
+        minValue: minValue(1),
+        maxValue: maxValue(1000),
+      },
+      background: {
+        maxLength: maxLength(250),
+      },
+    },
+  },
+  computed: {
+    nameErrors() {
+      const errors = [];
+      if (!this.$v.character.name.$dirty) return errors;
+      if (!this.$v.character.name.minLength) {
+        errors.push('Name must be at least 1 character.');
+      }
+      if (!this.$v.character.name.maxLength) {
+        errors.push('Name must not exceed 50 characters.');
+      }
+      if (!this.$v.character.name.isNameValid) {
+        errors.push('Name contains disallowed characters.');
+      }
+      if (!this.$v.character.name.required) {
+        errors.push('Name is required.');
+      }
+      return errors;
+    },
+    weightErrors() {
+      const errors = [];
+      if (!this.$v.character.weight.$dirty) return errors;
+      if (!this.$v.character.weight.integer) {
+        errors.push('Weight must be a number.');
+      }
+      if (!this.$v.character.weight.minValue) {
+        errors.push('Weight must be greater than 0 kg.');
+      }
+      if (!this.$v.character.weight.maxValue) {
+        errors.push('Weight must not exceed 1000 kg.');
+      }
+      if (!this.$v.character.weight.required) {
+        errors.push('Weight is required.');
+      }
+      return errors;
+    },
+    heightErrors() {
+      const errors = [];
+      if (!this.$v.character.height.$dirty) return errors;
+      if (!this.$v.character.height.integer) {
+        errors.push('Height must be a number.');
+      }
+      if (!this.$v.character.height.minValue) {
+        errors.push('Height must be greater than 0 cm.');
+      }
+      if (!this.$v.character.height.maxValue) {
+        errors.push('Height must not exceed 1000 cm.');
+      }
+      if (!this.$v.character.height.required) {
+        errors.push('Height is required.');
+      }
+      return errors;
+    },
+    backgroundErrors() {
+      const errors = [];
+      if (!this.$v.character.background.$dirty) return errors;
+      if (!this.$v.character.background.maxLength) {
+        errors.push('Background must not exceed 250 characters.');
+      }
+      return errors;
+    },
+  },
   methods: {
     /**
-     * Emits the up-to-date background data entered by the user.
-     * Should be used a click handler for the "next step" button.
+     * Check if the user has completed the step. If so, emits the
+     * up-to-date background data entered by the user. Should be used as
+     * a click handler for the "next step" button.
      */
     onComplete() {
+      this.$v.$touch();
+      if (this.$v.$invalid) return;
+
       this.$emit('complete', this.character);
     },
   },
